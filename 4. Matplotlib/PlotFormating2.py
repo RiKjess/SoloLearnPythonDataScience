96d5c84105a8248e3a4dc5d823fee4d1b22964ea
RiKjessLaptop <rikjessus@gmail.com>
2021-10-03 21:34:26 +0300

    add new practice

---
+++ 4. Matplotlib/PlotFormating2.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("https://www.sololearn.com/uploads/ca-covid.csv")
+
+df.drop('state', axis=1, inplace=True)
+df['date'] = pd.to_datetime(df['date'], format="%d.%m.%y")
+df['month'] = df['date'].dt.month
+df['day'] = df['date'].dt.day
+df.set_index('day', inplace=True)
+
+df = df[df['month']==6]
+
+df[['cases', 'deaths']].plot(kind="area", legend=True, stacked=False)
+plt.xlabel('Days in June')
+plt.ylabel('Number')
+plt.suptitle("COVID-19 in June")
+plt.show()
